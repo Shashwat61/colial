1094cf1a2f192b0360b0fbe1662952de8402d374
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-05-15 15:08:51 +0530

    third commit: set view engine and posts route and controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const homeController=require('../controllers/home_controller')
 console.log("routers loaded");
 
 
+
 router.get('/',homeController.home);
+router.use('/users',require('./users'))
+router.use('/posts',require('./posts'));
+//for any further routes access from here 
+//router.use('/routerName/,require('./routerfile))
+
+
 
 module.exports=router;
