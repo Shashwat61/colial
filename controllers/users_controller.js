c37ba40db54cb1550e37e36e16f87eda0ebe1d12
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-05-16 14:24:48 +0530

    sign up functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,51 @@
+const User = require("../models/user");
+
+
 module.exports.profile=function(req,res){
-   return  res.end('<h1>User Profile</h1>');
+   return  res.render('users',{
+      title:"users page"
+   });
+}
+//render signUp page
+module.exports.signUp=function(req,res){
+   return res.render('user_sign_up',{
+      title:"Colial | Sign Up"
+   })
 }
 
+//render signUp page
+module.exports.signIn=function(req,res){
+   return res.render('user_sign_in',{
+      title:"Colial | Sign In"
+   })
+}
+
+//get the sign up data
+
+module.exports.create=function(req,res){
+  if(req.body.password!=req.body.confirm_password){
+     return res.redirect('back')
+  }
+  User.findOne({email:req.body.email},function(err,user){
+     if(err){
+        console.log("error in finding user in signing up")
+        return;
+     }
+     if(!user){
+        User.create(req.body,function(err,user){
+         if(err){
+            console.log("error in finding user in signing up")
+            return;
+         }
+         return res.redirect('/users/sign-in');
+        })
+     }else{
+        return res.redirect('back');
+     }
+  })
+  
+}
+
+module.exports.createSession=function(req,res){
+
+}
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express=require('express')
-
-
 const app=express();
+const expressLayouts=require('express-ejs-layouts');
 const port=8000;
+const db=require('./config/mongoose')
+const cookieParser=require('cookie-parser');
+
+app.use(express.urlencoded())
+app.use(cookieParser())
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+//extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 
 //use express router
 app.use('/',require('./routes'));
+
 //set up the view-engine
-app.set('view-engine','ejs');
+app.set('view engine','ejs');
 app.set('views','./views')
+
 
 app.listen(port,(err)=>{
     if(err){
