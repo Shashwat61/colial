4cd53694020a86c7925eef9681f68687c7a5e3bb
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-05-16 17:37:05 +0530

    login done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,23 @@ const User = require("../models/user");
 
 
 module.exports.profile=function(req,res){
-   return  res.render('users',{
-      title:"users page"
-   });
+ if(req.cookies.user_id){
+    User.findById(req.cookies.user_id,function(err,user){
+       if(user){
+          return res.render('users',{
+             title:"User Profile",
+             user:user
+          })
+       }else{
+
+          return res.redirect('/users/sign-in')
+         }
+    })
+
+ }else{
+    return res.redirect('back')
+ }
+   
 }
 //render signUp page
 module.exports.signUp=function(req,res){
@@ -46,6 +60,33 @@ module.exports.create=function(req,res){
   
 }
 
+//sign in and create session for the usr
 module.exports.createSession=function(req,res){
+   //steps to authenticate       
+   //find the user 
+   User.findOne({email:req.body.email},function(err,user){
+      if(err){
+         console.log("error in finding user in signing in")
+         return;
+   }
 
+
+   //handle user found
+   if(user){
+      
+      //handle password which dont match
+      if(user.password!=req.body.password){
+         return res.redirect('back')
+      }
+      //handle session creation
+      res.cookie('user_id',user.id);
+      return res.redirect('/users/profile')
+
+          }else{
+              return res.redirect('back')
+          }
+
+
+          //handle user not found
+})
 }
