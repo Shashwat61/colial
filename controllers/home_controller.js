de4c846dd28eff7283b9738dbc26509dafdea623
shashwat <shashwat1461.cse18@chitkara.edu.in>
2021-05-18 13:24:24 +0530

    put level of  security onaction of post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,23 @@
+const Post=require('../models/posts')
+
 module.exports.home=function(req,res){
-    return res.render('home',{
-        title:"home"
+    
+    // Post.find({},function(err,post){
+
+        
+    //     return res.render('home',{
+    //         title:"Colial | home",
+    //         post:post
+    //     })
+    // }
+    // )
+   //popualate the user of each
+    Post.find({}).populate('user').exec(function(err,post){
+        console.log("populating")
+        return res.render('home',{
+            title:" Colial | Home ",
+            post:post
+        })
     })
 }
 
